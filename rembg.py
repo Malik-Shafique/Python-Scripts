45273331a4b139d5a88488417e6b5a43bf8eb5aa
Shafique Ur Rehman <shafique_mcs@hotmail.com>
2023-11-09 23:32:42 +0500

    Update rembg.py
    specify input and output path variables
    also convert to RGB

--- rembg.py
+++ rembg.py
@@ -1,15 +1,15 @@
 from PIL import Image
-input_path =  '/content/drive/MyDrive/pics/pic-sep-2023.jpg'
+input_path =  'input_image.jpg' # specify path to input image
 
 # Store path of the output image in the variable output_path
-output_path = '/content/drive/MyDrive/pics/pic-sep-2023-rembg.jpg'
+output_path = 'output_image.jpg'
 
 # Processing the image
 input = Image.open(input_path)
 
 # Removing the background from the given Image
 output = remove(input)
-
+#convert image to RGB, because jpeg format uses RGB
 output = output.convert('RGB')
 #Saving the image in the given path
 output.save(output_path)
