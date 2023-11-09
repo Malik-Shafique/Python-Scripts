6857a0d175f88128a8c976b59a3ac07990c92abc
Shafique Ur Rehman <shafique_mcs@hotmail.com>
2023-11-09 23:26:30 +0500

    Update rembg.py
    Using PIL to remove background

--- rembg.py
+++ rembg.py
@@ -1,1 +1,15 @@
 from PIL import Image
+input_path =  '/content/drive/MyDrive/pics/pic-sep-2023.jpg'
+
+# Store path of the output image in the variable output_path
+output_path = '/content/drive/MyDrive/pics/pic-sep-2023-rembg.jpg'
+
+# Processing the image
+input = Image.open(input_path)
+
+# Removing the background from the given Image
+output = remove(input)
+
+output = output.convert('RGB')
+#Saving the image in the given path
+output.save(output_path)
