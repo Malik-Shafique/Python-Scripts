a38ee2315fc0ef987b97badbea126f042cc6e1b2
Shafique Ur Rehman <shafique_mcs@hotmail.com>
2023-11-26 19:19:07 +0500

    Update grayscale_opencv.py to read and display image

--- grayscale_opencv.py
+++ grayscale_opencv.py
@@ -2,5 +2,12 @@
 
 import cv2
 
+#read image file any 'my_image.jpg' (name of file) in same folder as grayscale_opencv.py
+img_cv2 = cv2.imread('my_image.jpg',0)    # 2nd argument 0 means to read image as grayscale
+
+cv2.imshow('My Image',img_cv2)
+ 
+cv2.waitKey(0) # to waits until a key is pressed
+cv2.destroyAllWindows() # it destroys the window displaying image
 
 
