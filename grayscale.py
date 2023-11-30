ad2cb88d8066a40e98b172038b58c84703bef9b2
Shafique Ur Rehman <shafique_mcs@hotmail.com>
2023-11-30 21:46:54 +0500

    change image file name in grayscale.py
    name according to available image name

--- grayscale.py
+++ grayscale.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import matplotlib.pyplot as plt  
 
-im_path = 'color.jpg'  #change path to your image file
+im_path = 'my_image.jpg'  #change path to your image file
 pil_image = Image.open(im_path).convert('L')   # open image file and convert to grayscale
 pil_image.save('grayscale.jpg')  # save file at desired location
 
